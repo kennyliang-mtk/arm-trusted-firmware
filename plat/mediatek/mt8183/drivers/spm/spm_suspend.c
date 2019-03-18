d3f89c001dbc5f448aea1a8881359e26002904d7
Roger Lu <roger.lu@mediatek.com>
2019-03-18 10:41:41 +0800

    mediatek/mt8183: spm: align the * on each line
    Change-Id: I756a727aaaf19459d698f17af94f63192e97757d
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>

--- plat/mediatek/mt8183/drivers/spm/spm_suspend.c
+++ plat/mediatek/mt8183/drivers/spm/spm_suspend.c
@@ -1,8 +1,8 @@
 /*
-* Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
-*
-* SPDX-License-Identifier: BSD-3-Clause
-*/
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 #include <arch_helpers.h>
 #include <debug.h>
--- plat/mediatek/mt8183/drivers/spm/spm_suspend.h
+++ plat/mediatek/mt8183/drivers/spm/spm_suspend.h
@@ -1,8 +1,8 @@
 /*
-* Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
-*
-* SPDX-License-Identifier: BSD-3-Clause
-*/
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 #ifndef __SPM_SUSPEND_H__
 #define __SPM_SUSPEND_H__
