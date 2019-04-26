ad266b0793d79a5f8091b662853668117a8744be
Roger Lu <roger.lu@mediatek.com>
2019-04-26 15:45:06 +0800

    mediatek/mt8183: spm_pmic_pwrap: add spm_pmic_pwrap table
    BUG=b:128875656
    
    Change-Id: I7bc9335aba2961459fe96ae10a33de0e015735d2
    Signed-off-by: Roger Lu <roger.lu@mediatek.com>

--- plat/mediatek/mt8183/platform.mk
+++ plat/mediatek/mt8183/platform.mk
@@ -45,9 +45,10 @@ BL31_SOURCES    += drivers/arm/cci/cci.c                          \
                    ${MTK_PLAT_SOC}/drivers/mcsi/mcsi.c            \
                    ${MTK_PLAT_SOC}/drivers/pmic/pmic_wrap_init.c  \
                    ${MTK_PLAT_SOC}/drivers/pmic/pmic.c            \
-                   ${MTK_PLAT_SOC}/drivers/rtc/rtc.c            \
+                   ${MTK_PLAT_SOC}/drivers/rtc/rtc.c              \
                    ${MTK_PLAT_SOC}/drivers/spmc/mtspmc.c          \
                    ${MTK_PLAT_SOC}/drivers/spm/spm.c              \
+                   ${MTK_PLAT_SOC}/drivers/spm/spm_pmic_wrap.c    \
                    ${MTK_PLAT_SOC}/drivers/spm/spm_suspend.c      \
                    ${MTK_PLAT_SOC}/drivers/gpio/mtgpio.c          \
                    ${MTK_PLAT_SOC}/plat_pm.c                      \
