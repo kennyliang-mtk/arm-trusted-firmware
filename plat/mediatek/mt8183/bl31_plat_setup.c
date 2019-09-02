4a8f7329cdc2c91797bb670e61f82c49bfa60ff5
kenny liang <kenny.liang@mediatek.com>
2019-09-02 21:13:57 +0800

    mediatek: mt8183: correct dram protection setting
    spm/sspm: forbidden all dram access
    cm4:      can RW access in 0x50000000~0x52900000 only
    
    Signed-off-by: kenny liang <kenny.liang@mediatek.com>

--- plat/mediatek/mt8183/bl31_plat_setup.c
+++ plat/mediatek/mt8183/bl31_plat_setup.c
@@ -146,15 +146,14 @@ void bl31_platform_setup(void)
 	devapc_init();
 
 	/* set permission */
-	/* for DOMAIN1: only can access 0x50000000 ~ 0x52900000(use region0) */
-	emi_mpu_set_region_protection(
-		0x50000000, 0x52900000, 0, (SEC_RW_NSEC_W << 3));
-	/* for DOMAIN1/2: forbidden all access to DRAM (use region1/region2) */
-	emi_mpu_set_region_protection(
-		0x40000000UL, 0x60000000UL, 1, (FORBIDDEN << 3 | FORBIDDEN << 6));
-	emi_mpu_set_region_protection(
-		0x60000000UL, 0x80000000UL, 2, (FORBIDDEN << 3 | FORBIDDEN << 6));
-
+	emi_mpu_set_region_protection(0x40000000UL, 0x50000000UL, 0,
+				(FORBIDDEN << 3 | FORBIDDEN << 6));
+	emi_mpu_set_region_protection(0x50000000UL, 0x52900000UL, 1,
+				(FORBIDDEN << 6));
+	emi_mpu_set_region_protection(0x52900000UL, 0x60000000UL, 2,
+				(FORBIDDEN << 3 | FORBIDDEN << 6));
+	emi_mpu_set_region_protection(0x60000000UL, 0x80000000UL, 3,
+				(FORBIDDEN << 3 | FORBIDDEN << 6));
 	dump_emi_mpu_regions();
 
 	platform_setup_cpu();
